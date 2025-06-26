9b1c2319671fa27de63e7dea433f5269357fe421
Lisette Joyce AZONDEKON <57040343+LisetteJoyce@users.noreply.github.com>
2025-06-26 04:47:42 +0000

    Added Vehicle and MaintenanceTask models

---
+++ manage.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Vehicle(models.Model):
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.year} {self.make} {self.model}"
+
+class MaintenanceTask(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    task_name = models.CharField(max_length=100)
+    steps = models.TextField()
+    tools_required = models.TextField()
+    parts_required = models.TextField()
+    estimated_time = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.task_name} for {self.vehicle}"
